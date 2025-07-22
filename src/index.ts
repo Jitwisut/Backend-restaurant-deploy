7961f3bc5597e3162c3f4b5545b62867764bdb61
Jitwisut <tanzajitwisut@gmail.com>
2025-07-22 15:19:55 +0700

    update Auth2

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ const app = new Elysia();
 app
   .use(
     cors({
-      origin: url2,
+      origin: url,
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
       allowedHeaders: ["Content-Type", "Authorization", "X-XSRF-TOKEN"],
